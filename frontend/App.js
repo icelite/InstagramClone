456a1d6f3b61a030d52a8a42e3c4208fc9db3183
icelite <91568106+icelite@users.noreply.github.com>
2022-05-18 18:03:33 -0400

    f i r e b a s e

--- frontend/App.js
+++ frontend/App.js
@@ -33,14 +33,14 @@ console.warn = message => {
 };
 
 const firebaseConfig = {
-  apiKey: "****",
-  authDomain: "****",
-  databaseURL: "****",
-  projectId: "****",
-  storageBucket: "****",
-  messagingSenderId: "****",
-  appId: "****",
-  measurementId: "****"
+  apiKey: "AIzaSyBHxnRKPKTqnt5x4Aw1pg8_IM8tD5QLAQU",
+  authDomain: "instapost-id.firebaseapp.com",
+  databaseURL: "https://instapost-id-default-rtdb.firebaseio.com",
+  projectId: "instapost-id",
+  storageBucket: "instapost-id.appspot.com",
+  messagingSenderId: "690177035862",
+  appId: "1:690177035862:web:74f96795cb43f1ea1635f7",
+  measurementId: "G-KVN82029KS"
 };
 
 const logo = require('./assets/logo.png')
